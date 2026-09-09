541a8e94042c69505f3c3863e6ba7ac1e33a0e47
MariamAlix24 <alimariem59@gmail.com>
2026-09-09 22:49:04 +0300

    Add project files.

---
+++ G-ASP-NET-99-OOP-01/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_ASP_NET_99_OOP_01
+{
+public struct DeliveryAddress
+    {
+        public string city;
+        public string street;
+    }
+    public class Customer
+    {
+        public string name;
+      
+    }
+}
---
+++ G-ASP-NET-99-OOP-01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace G_ASP_NET_99_OOP_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
